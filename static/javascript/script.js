9429c5e3687b0e1b48609e445dcccfa47c57b2c1
PhucTran <tranbaophuc2703@gmail.com>
2024-07-11 15:48:35 +0700

    update

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,5 +1,8 @@
 document.getElementById('upload-form').addEventListener('submit', function(event) {
   event.preventDefault();
+  const resultDiv = document.getElementById('result-json');
+  resultDiv.innerHTML = '<div> Processing... </div>';
+
   const formData = new FormData(this);
   fetch('/upload', {
     method: 'POST',
@@ -7,10 +10,26 @@ document.getElementById('upload-form').addEventListener('submit', function(event
   })
   .then(response => response.json())
   .then(data => {
-    const resultDiv = document.getElementById('result-json');
     resultDiv.innerHTML = '<pre>' + JSON.stringify(data,null,2) + '</pre>';
   })
   .catch(error => {
     console.error('Error:', error);
   });
 });
+
+function previewImage(event) {
+  const resultDiv = document.getElementById('tempImage');
+  resultDiv.innerHTML = ''; // Clear previous content
+
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const img = document.createElement('img');
+      img.src = e.target.result;
+      img.classList.add('mt-4', 'w-full', 'rounded-lg');
+      resultDiv.appendChild(img);
+    };
+    reader.readAsDataURL(file);
+  }
+}
